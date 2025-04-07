03ce35927d97e4a7b79219f1f05217c584ea6499
spencer-t-york <spencer.t.york@gmail.com>
2025-04-07 01:36:29 +0000

    get rid of errors on wbsite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['workouttracker.publicvm.com'],
+    strictPort: true,
+    origin: 'https://workouttracker.publicvm.com',
+    hmr: {
+      protocol: 'wss',
+      host: 'workouttracker.publicvm.com',
+    },
   },
-});
+})
