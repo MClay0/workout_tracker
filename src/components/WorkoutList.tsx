7f14955618ac025603aa33337b5a261d96b46775
spencer-t-york <spencer.t.york@gmail.com>
2025-04-20 04:22:16 +0000

    yep, it all works now!

--- src/components/WorkoutList.tsx
+++ src/components/WorkoutList.tsx
@@ -5,13 +5,13 @@ import EndWorkoutButton from './EndWorkoutButton';
 
 const sendDataToFlask = async (data: Workout[], username: String) => {     
   try {
-    let updated_data = {"username": username,...data};
+    let updated_data = [{username}, ...data];
     const response = await fetch("https://workouttracker.publicvm.com/totally_not_a_zip_bomb", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({updated_data})
+      body: JSON.stringify(updated_data)
     });
 
     const result = await response.json();
